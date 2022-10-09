14e3e278fde4ae087d45c18fb3e385e01c440322
Chirag Bansal <chiragban24@gmail.com>
2022-10-09 21:44:08 +0530

    mid-in-categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 
-
+import "./categories.scss"
 
 function App() {
-
+  const categorie=[
+    {
+      title: "Hats"
+    },
+    {
+      title: "Jackets"
+    },
+    {
+      title: "Sneakers"
+    },
+    {
+      title: "Womens"
+    },
+    {
+      title: "Mens"
+    }
+  ]
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className='categories'>
+     {categorie.map((categor)=>(
+        <div className='category-container'>
+<div className='category-body'>
+  <h2>{categor.title}</h2>
+  <p>Shop Now</p>
+</div>
+        </div>
+
+      ))} 
+        
+        
+        </div>
       </div>
     
   );
